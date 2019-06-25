ce8f0ac0f0f014c272e1cb83ff83622848b4391d
wbruey <willbruey@gmail.com>
2019-06-25 13:28:16 -0400

    i created a simple hello world cpp files and header files as an example getting used to cpp again

---
+++ hellofunc.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+#include "hellohead.h"
+
+void myPrintHelloWorld(void) {
+
+  printf("Hello bruester!\n");
+  std::cout << "Hello, World!\n";
+
+  return;
+}
---
+++ hellohead.h
@@ -0,0 +1,5 @@
+/*
+example include file
+*/
+
+void myPrintHelloWorld(void);
