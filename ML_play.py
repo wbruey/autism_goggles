cd4ed595bb8c1b83d4a8996cecd32f61b2d2ff76
wbruey <willbruey@gmail.com>
2019-08-21 12:37:51 -0400

    got data from brian and seyka

---
+++ ML_play.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+# load dataset into Pandas DataFrame
+df = pd.read_csv(url, names=['sepal length','sepal width','petal length','petal width','target'])
+print(df)
+
+
+features = ['sepal length', 'sepal width', 'petal length', 'petal width']
+# Separating out the features
+x = df.loc[:, features].values
+# Separating out the target
+y = df.loc[:,['target']].values
+# Standardizing the features
+x = StandardScaler().fit_transform(x)
+
+print(x)
+print(y)
